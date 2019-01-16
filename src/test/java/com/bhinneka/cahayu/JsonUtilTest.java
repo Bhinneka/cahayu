4d666cad0bbaf79bd352d93d180e186eb67b51cb
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-16 16:34:52 +0700

    add json util test

---
+++ src/test/java/com/bhinneka/cahayu/JsonUtilTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhinneka.cahayu;
+
+import com.bhinneka.cahayu.modules.user.model.User;
+import com.bhinneka.cahayu.shared.JsonUtil;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ *
+ * @author wurianto
+ */
+public class JsonUtilTest extends TestCase{
+    
+    public JsonUtilTest(String testName) {
+        super(testName);
+    }
+    
+    public static Test suite() {
+        return new TestSuite(JsonUtilTest.class);
+    }
+    
+    public void testJsonUtil() {
+        String jsonString = "{ \"id\": \"USR002\",  \"firstName\": \"James\", \"lastName\": \"Gosling\",  \"email\": \"james@bhinneka.com\"}"; 
+        try {
+            User u = JsonUtil.jsonToData(User.class, jsonString.getBytes());
+            String expectedFirstName = "James";
+            
+            assertEquals(expectedFirstName, u.getFirstName());
+        } catch (IOException ex) {
+            Logger.getLogger(JsonUtilTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
