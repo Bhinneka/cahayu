dde090c3a4fd488151986f28c4789e86fde83a8a
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-16 17:26:41 +0700

    move server construct to Server.java

--- src/main/java/com/bhinneka/cahayu/App.java
+++ src/main/java/com/bhinneka/cahayu/App.java
@@ -1,18 +1,10 @@
 package com.bhinneka.cahayu;
 
-import com.bhinneka.cahayu.modules.user.delivery.SparkHandler;
-import com.bhinneka.cahayu.modules.user.model.User;
-import com.bhinneka.cahayu.modules.user.repository.IUserRepository;
-import com.bhinneka.cahayu.modules.user.repository.UserRepositoryInMem;
-import com.bhinneka.cahayu.modules.user.usecase.IUserUsecase;
-import com.bhinneka.cahayu.modules.user.usecase.UserUsecaseImpl;
 import io.github.cdimascio.dotenv.Dotenv;
-import java.util.HashMap;
-import java.util.Map;
-import spark.Spark;
+
 
 /**
- * created by Wuriyanto 16 01n2019
+ * created by Wuriyanto 16 01 2019
  *
  */
 public class App {
@@ -21,23 +13,7 @@ public class App {
         Dotenv dotenv = Dotenv.load();
         int port = Integer.parseInt(dotenv.get("PORT"));
         
-        //set custom port
-        Spark.port(port);
-        
-        // user module
-        Map<String, User> db = new HashMap<>();
-        db.put("USR001", new User("USR001", "Wuriyanto", "Musobar", "wuriyanto@bhinneka.com"));
-        db.put("USR002", new User("USR002", "James", "Gosling", "james@bhinneka.com"));
-        
-        IUserRepository userRepository = new UserRepositoryInMem(db);
-        IUserUsecase userUsecase = new UserUsecaseImpl(userRepository);
-        
-        //user handler
-        SparkHandler userSparkHandler = new SparkHandler(userUsecase);
-        
-        Spark.get("/", new IndexRoute());
-        Spark.get("/users", userSparkHandler.index());
-        Spark.post("/users", userSparkHandler.addUser());
-        
+        Server server = new Server(port);
+        server.start();
     }
 }
--- src/main/java/com/bhinneka/cahayu/Server.java
+++ src/main/java/com/bhinneka/cahayu/Server.java
@@ -5,11 +5,51 @@
  */
 package com.bhinneka.cahayu;
 
+import com.bhinneka.cahayu.modules.user.delivery.SparkHandler;
+import com.bhinneka.cahayu.modules.user.model.User;
+import com.bhinneka.cahayu.modules.user.repository.IUserRepository;
+import com.bhinneka.cahayu.modules.user.repository.UserRepositoryInMem;
+import com.bhinneka.cahayu.modules.user.usecase.IUserUsecase;
+import com.bhinneka.cahayu.modules.user.usecase.UserUsecaseImpl;
+import com.bhinneka.cahayu.shared.Filters;
+import java.util.HashMap;
+import java.util.Map;
+import spark.Spark;
+
 /**
  *
  * @author wurianto
  */
 public class Server {
+    
+    private final int port;
+    
+    public Server(int port) {
+        this.port = port;
+    }
+    
+    public void start() {
+        //set custom port
+        Spark.port(port);
+        
+        // set filters
+        Spark.before("/users", Filters.setJsonHeader());
+        
+        // user module
+        Map<String, User> db = new HashMap<>();
+        db.put("USR001", new User("USR001", "Wuriyanto", "Musobar", "wuriyanto@bhinneka.com"));
+        db.put("USR002", new User("USR002", "James", "Gosling", "james@bhinneka.com"));
+        
+        IUserRepository userRepository = new UserRepositoryInMem(db);
+        IUserUsecase userUsecase = new UserUsecaseImpl(userRepository);
+        
+        //user handler
+        SparkHandler userSparkHandler = new SparkHandler(userUsecase);
+        
+        Spark.get("/", new IndexRoute());
+        Spark.get("/users", userSparkHandler.index());
+        Spark.post("/users", userSparkHandler.addUser());
+    }
     
     
 }
