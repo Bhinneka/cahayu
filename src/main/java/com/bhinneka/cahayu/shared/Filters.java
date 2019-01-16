97e48af09d37936a11f1b2c357849fbfc1d182c7
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-16 17:25:54 +0700

    add Filters

---
+++ src/main/java/com/bhinneka/cahayu/shared/Filters.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhinneka.cahayu.shared;
+
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+
+/**
+ *
+ * @author wurianto
+ */
+public class Filters {
+    
+    public static Filter setJsonHeader() {
+        return (Request req, Response res) -> {
+          res.header("Content-Type", "application/json");  
+        };
+    }
+}
