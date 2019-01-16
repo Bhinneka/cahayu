fa219dbc2e3225b10b3571bcf747089af21730aa
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-16 10:59:53 +0700

    refactor json util

--- src/main/java/com/bhinneka/cahayu/shared/JsonUtil.java
+++ src/main/java/com/bhinneka/cahayu/shared/JsonUtil.java
@@ -5,20 +5,26 @@
  */
 package com.bhinneka.cahayu.shared;
 
-import com.google.gson.Gson;
-import spark.ResponseTransformer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.IOException;
+import java.io.StringWriter;
 
 /**
  *
  * @author wurianto
  */
-public class JsonUtil implements ResponseTransformer{
-    
-    private final Gson gson = new Gson();
 
-    @Override
-    public String render(Object o) throws Exception {
-        return gson.toJson(o);
+public class JsonUtil {
+    public static String dataToJson(Object data) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            StringWriter sw = new StringWriter();
+            mapper.writeValue(sw, data);
+            return sw.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("IOEXception while mapping object (" + data + ") to JSON");
+        }
     }
-    
 }
