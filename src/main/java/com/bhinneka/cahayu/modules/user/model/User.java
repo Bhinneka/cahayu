a663168fc58ab2ec34e8f3ee38cf3c0324db7d02
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-16 11:00:42 +0700

    add default constructor

--- src/main/java/com/bhinneka/cahayu/modules/user/model/User.java
+++ src/main/java/com/bhinneka/cahayu/modules/user/model/User.java
@@ -10,11 +10,18 @@ package com.bhinneka.cahayu.modules.user.model;
  * @author wurianto
  */
 public class User {
-    
+
     private String id;
     private String firstName;
     private String lastName;
     private String email;
+
+    public User(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
     public String getId() {
         return id;
@@ -47,6 +54,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-    
-    
+
 }
