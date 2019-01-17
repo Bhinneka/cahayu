5d5928722ef143c54ab1199001db62da393eca92
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-17 23:59:04 +0700

    add custom response

---
+++ src/main/java/com/bhinneka/cahayu/CustomResponse.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhinneka.cahayu;
+
+/**
+ *
+ * @author wurianto
+ */
+public class CustomResponse {
+
+    private Integer code;
+    private boolean success;
+    private Object data;
+    private String message;
+
+    public CustomResponse() {
+
+    }
+
+    public CustomResponse(Integer code, boolean success, Object data, String message) {
+        this.code = code;
+        this.success = success;
+        this.data = data;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
