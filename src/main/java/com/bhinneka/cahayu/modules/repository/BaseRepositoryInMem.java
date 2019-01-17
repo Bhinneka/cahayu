93fa1de6f6d4898998eb2f9060a54a1cfb10f454
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-17 20:56:20 +0700

    add base repo in memory implementation

---
+++ src/main/java/com/bhinneka/cahayu/modules/repository/BaseRepositoryInMem.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhinneka.cahayu.modules.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author wurianto
+ * @param <T>
+ * @param <K>
+ */
+public class BaseRepositoryInMem<T, K> implements IBaseRepository<T, K>{
+    
+    private final Map<K, T> db;
+    
+    public BaseRepositoryInMem(Map<K, T> db) {
+        this.db = db;
+    }
+
+    @Override
+    public T save(T t) {
+        return t;
+    }
+
+    @Override
+    public T delete(K id) {
+        T u = this.db.remove(id);
+        return u;
+    }
+
+    @Override
+    public T findById(K id) {
+        return this.db.get(id);
+    }
+    
+    
+    @Override
+    public List<T> findAll() {
+        return new ArrayList<>(db.values());
+    }
+    
+}
