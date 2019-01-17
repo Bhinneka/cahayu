f649f9fd019f00b99db506254044e6fbe88504ba
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-17 17:36:35 +0700

    update i user repository, to inherit I generic repository

--- src/main/java/com/bhinneka/cahayu/modules/user/repository/IUserRepository.java
+++ src/main/java/com/bhinneka/cahayu/modules/user/repository/IUserRepository.java
@@ -5,6 +5,7 @@
  */
 package com.bhinneka.cahayu.modules.user.repository;
 
+import com.bhinneka.cahayu.modules.repository.IGenericRepository;
 import com.bhinneka.cahayu.modules.user.model.User;
 import java.util.List;
 
@@ -12,11 +13,17 @@ import java.util.List;
  *
  * @author wurianto
  */
-public interface IUserRepository {
-    
+public interface IUserRepository extends IGenericRepository<User, String> {
+
+    @Override
     public User save(User u);
+
+    @Override
     public User findById(String id);
+
     public User findByEmail(String email);
+
+    @Override
     public List<User> findAll();
-    
+
 }
