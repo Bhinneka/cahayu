ae6485e7b7a236ba804864d88e81bbd832f81d0e
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-17 17:10:19 +0700

    add generic repository

---
+++ src/main/java/com/bhinneka/cahayu/modules/repository/IGenericRepository.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bhinneka.cahayu.modules.repository;
+
+import java.util.List;
+
+/**
+ *
+ * @author wurianto
+ *
+ * @param <T>
+ * @param <K>
+ */
+public interface IGenericRepository<T, K> {
+
+    public T save(T t);
+
+    public T findById(K id);
+
+    public T delete(K id);
+
+    public List<T> findAll();
+
+}
