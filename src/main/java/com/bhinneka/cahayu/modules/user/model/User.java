6d0ec3ea9ed207e22b372f41ead2715913c46967
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-18 00:05:44 +0700

    add password field

--- src/main/java/com/bhinneka/cahayu/modules/user/model/User.java
+++ src/main/java/com/bhinneka/cahayu/modules/user/model/User.java
@@ -15,16 +15,18 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
 
     public User() {
 
     }
 
-    public User(String id, String firstName, String lastName, String email) {
+    public User(String id, String firstName, String lastName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.password = password;
     }
 
     public String getId() {
@@ -57,6 +59,18 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public boolean validatePassword(String password) {
+        return this.password.equals(password);
     }
 
 }
