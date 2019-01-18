4f5cb1d5fc4167d1e53d798952f8ed343a6a0aec
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-18 08:00:09 +0700

    app using auto generated key for testing

--- src/main/java/com/bhinneka/cahayu/App.java
+++ src/main/java/com/bhinneka/cahayu/App.java
@@ -6,6 +6,10 @@ import com.bhinneka.cahayu.jwt.JwtServiceImpl;
 import com.bhinneka.cahayu.jwt.KeyReader;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.impl.crypto.RsaProvider;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.Key;
 import java.security.KeyPair;
 import java.util.Calendar;
@@ -17,33 +21,43 @@ import java.util.Date;
  */
 public class App {
 
-    String privateKeyPath = getClass().getClassLoader().getResource("rsapriv.der").getPath();
-    String publicKeyPath = getClass().getClassLoader().getResource("rsapub.der").getPath();
-
+    //String privateKeyPath = getClass().getResource("target/clasess/rsapriv.der").getPath();
+    //String publicKeyPath = getClass().getResource("target/clasess/rsapub.der").getPath();
     public static void main(String[] args) throws Exception {
         Dotenv dotenv = Dotenv.load();
         int port = Integer.parseInt(dotenv.get("PORT"));
 
-        Server server = new Server(port);
-        server.start();
+        App app = new App();
 
-//        App app = new App();
-//        
-//        KeyPair kp = RsaProvider.generateKeyPair();
-//        Key privateKey = KeyReader.getPrivateKey(app.privateKeyPath);
-//        Key publicKey = KeyReader.getPublicKey(app.publicKeyPath);
+//        InputStream privateKeyInput = App.class.getResourceAsStream("rsapriv.der");
+//        InputStream publicKeyInput = App.class.getResourceAsStream("rsapub.der");
+//
+//        String privateKeyPath = App.class.getResourceAsStream("rsapriv.der");
+//        String publicKeyPath = App.class.getResourceAsStream("rsapub.der");
+//
+//        Key privateKey = KeyReader.getPrivateKey(privateKeyPath);
+//        Key publicKey = KeyReader.getPublicKey(publicKeyPath);
+
+        // ya ya
+        KeyPair kp = RsaProvider.generateKeyPair();
+        // generate private and public key
+        Key privateKey = kp.getPrivate();
+        Key publicKey = kp.getPublic();
+
+        Server server = new Server(port, privateKey, publicKey);
+        server.start();
 //        
 //        
 //        IJwtService jwtService = new JwtServiceImpl(privateKey, publicKey);
 //
 //        Calendar cal = Calendar.getInstance();
+//        cal.set(Calendar.MONTH, Calendar.AUGUST);
+//        cal.set(Calendar.YEAR, 2019);
+//        cal.set(Calendar.DATE, 13);
 //        cal.set(Calendar.HOUR_OF_DAY, 17);
 //        cal.set(Calendar.MINUTE, 30);
 //        cal.set(Calendar.SECOND, 0);
 //        cal.set(Calendar.MILLISECOND, 0);
-//        cal.set(Calendar.MONTH, Calendar.AUGUST);
-//        cal.set(Calendar.YEAR, 2019);
-//        cal.set(Calendar.DATE, 13);
 //
 //        Date d = cal.getTime();
 //        String jwtToken = jwtService.generate(new CustomClaim("001", "bhinneka.com", "my-service", cal.getTime()));
@@ -56,5 +70,16 @@ public class App {
 //        } catch (Exception e) {
 //            System.out.println(e);
 //        }
+    }
+
+    private String readFromInputStream(InputStream inputStream) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
     }
 }
