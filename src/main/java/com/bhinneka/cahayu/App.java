5775e80dcd62f739c165e4dcd2b50efd1e54d1e6
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-21 17:11:40 +0700

    add license to main java

--- src/main/java/com/bhinneka/cahayu/App.java
+++ src/main/java/com/bhinneka/cahayu/App.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 wuriyanto.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bhinneka.cahayu;
 
 import com.bhinneka.cahayu.jwt.KeyReader;
@@ -38,7 +53,7 @@ public class App {
         Server server = new Server(port, privateKey, publicKey);
         server.setMongoDbConnectionString(mongoDbConnectionString);
         server.setMongoDbDatabaseName(mongoDbDatabaseName);
-                
+
         server.start();
     }
 }
